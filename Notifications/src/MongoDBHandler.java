c8ba1d4ccc6e78ed3b2df19d8a25944d1d4a05f5
Willie <44509127+Kakkarrot@users.noreply.github.com>
2020-03-11 16:15:34 -0600

    completed mongodb setup to driver

--- Notifications/src/MongoDBHandler.java
+++ Notifications/src/MongoDBHandler.java
@@ -1,7 +1,37 @@
+import java.util.Arrays;
+
 import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 
 public class MongoDBHandler {
 	
+	MongoClient mongoClient;
+	MongoDatabase database;
 	
+	public MongoDBHandler() {
+		establishConnection();
+		database = null;
+	}
 	
+	public void addNotification(MyNotification notification) {
+		
+	}
+	
+	private void establishConnection() {
+		mongoClient = MongoClients.create(
+				"mongodb+srv://SENG401:seng401@cluster0-hcvzz.mongodb.net/test?retryWrites=true&w=majority");
+		database = mongoClient.getDatabase("test");		
+	}
+	
+	private void setDatabase(String db_name) {
+		database = mongoClient.getDatabase(db_name);
+	}
+	
+	private void closeConnection() {
+		mongoClient.close();
+	}
 }
