61267da027bf2056edd57058a374d0d15703c556
Willie <44509127+Kakkarrot@users.noreply.github.com>
2020-03-11 12:36:53 -0600

    made some notes

---
+++ Notifications/src/runNotifications.java
@@ -0,0 +1,26 @@
+/*
+ * Store notifications in database
+ * Every time someone makes a post on a forum, a notification will be added to the database
+ * 
+ * FIRST METHOD - add notifications into the database by id for each user in the given list
+ * 
+ * Given one user, find all the notifications and sort them in queued order using id
+ * 
+ * SECOND METHOD - retrieve all notifications in the database
+ * 
+ * When the user sees notifications, the flag on notifications must change
+ * 
+ * THIRD METHOD - 
+ * 
+ * 
+ */
+
+
+public class runNotifications {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
