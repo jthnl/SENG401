f4d1f6ab0160952f88ea4aa451c59943aa52b231
Jathniel Ong <ong.jathniel@gmail.com>
2020-03-20 20:27:59 -0600

    Initialized Main Web-Application
    Created a new Maven Project. Currently initialized with Java Spring and
    protobufs.

---
+++ main-webapp/nhlweb/src/main/java/com/nhl/nhlweb/NhlwebApplication.java
@@ -0,0 +1,13 @@
+package com.nhl.nhlweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhlwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhlwebApplication.class, args);
+	}
+
+}
---
+++ main-webapp/nhlweb/src/test/java/com/nhl/nhlweb/NhlwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhl.nhlweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhlwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
