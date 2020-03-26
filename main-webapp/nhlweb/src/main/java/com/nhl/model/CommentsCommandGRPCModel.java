3d6071e751486953216538402c17f564e44f1b9b
Owen Troke-Billard <owen.trokebillard@outlook.com>
2020-03-26 00:32:44 -0600

    Add comments microservice GRPC to main app

---
+++ main-webapp/nhlweb/src/main/java/com/nhl/model/CommentsCommandGRPCModel.java
@@ -0,0 +1,30 @@
+package com.nhl.model;
+
+import com.comments.commentsproto.AddCommentCommand;
+import com.comments.commentsproto.CommandServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class CommentsCommandGRPCModel {
+    private final CommandServiceGrpc.CommandServiceBlockingStub commandStub;
+
+    public CommentsCommandGRPCModel() {
+        ManagedChannel channel = ManagedChannelBuilder
+                .forAddress("localhost", 50052)
+                .usePlaintext()
+                .build();
+
+        commandStub = CommandServiceGrpc.newBlockingStub(channel);
+    }
+
+    public void addComment(String postId, String content){
+        AddCommentCommand request = AddCommentCommand
+                .newBuilder()
+                .setPostId(postId)
+                .setContent(content)
+                .build();
+
+        //noinspection ResultOfMethodCallIgnored
+        commandStub.addComment(request);
+    }
+}
