f600ab01712322d31f9ecda9e054c8dae0928ed9
Owen Troke-Billard <owen.trokebillard@outlook.com>
2020-04-02 22:07:22 -0600

    Add indentation to comments

--- main-webapp/nhlweb/src/main/java/com/nhl/view/CommentView.java
+++ main-webapp/nhlweb/src/main/java/com/nhl/view/CommentView.java
@@ -14,8 +14,9 @@ public class CommentView implements MsgObjectView {
     public int downvotes;
     public List<CommentView> nested;
     public boolean isNested;
+    public int indentation;
 
-    public CommentView(String id, String parentId, String authorId, String content, int upvotes, int downvotes, List<CommentView> nested, boolean isNested) {
+    public CommentView(String id, String parentId, String authorId, String content, int upvotes, int downvotes, List<CommentView> nested, boolean isNested, int indentation) {
         this.id = id;
         this.parentId = parentId;
         this.authorId = authorId;
@@ -24,23 +25,29 @@ public class CommentView implements MsgObjectView {
         this.downvotes = downvotes;
         this.nested = nested;
         this.isNested = isNested;
+        this.indentation = indentation;
     }
 
     public CommentView(Comment comment) {
+        this(comment, 0);
+    }
+
+    public CommentView(Comment comment, int indentation) {
         this.id = comment.getId();
         this.parentId = comment.getParentId();
         this.authorId = comment.getAuthorId();
         this.content = comment.getContent();
         this.upvotes = comment.getUpvotes();
         this.downvotes = comment.getDownvotes();
-        this.nested = getNestedComments(comment);
+        this.nested = getNestedComments(comment, indentation);
         this.isNested = comment.getIsNested();
+        this.indentation = indentation;
     }
 
-    private List<CommentView> getNestedComments(Comment comment){
+    private List<CommentView> getNestedComments(Comment comment, int indentation){
         List<CommentView> nested = new ArrayList<>(comment.getNestedCount());
         for (Comment nestedComment : comment.getNestedList()){
-            CommentView nestedView = new CommentView(nestedComment);
+            CommentView nestedView = new CommentView(nestedComment, indentation + 1);
             nested.add(nestedView);
         }
         return nested;
